00ce8bcf9ec6df618b7a44fa73d9336a6795046b
Nerezea <nerezea@outlook.de>
2023-11-27 15:02:02 +0100

    cleanup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./controller/App.jsx";
 import "./css/main.css";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
+
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    {/* <Provider> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
