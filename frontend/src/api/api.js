ba137969a76c094fb07d3098c98f5b9e1dee169c
shahab NG <nagibi.shahab@gmail.com>
2023-12-12 14:53:07 +0100

    port

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "/api"
-      : "http://localhost:4040/api",
+      : "http://localhost:5000/api",
   headers: {
     token: localStorage.getItem("token"),
   },
