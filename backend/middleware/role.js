7d5614063a78aff1103a1bcd219b38d33dc5f149
newmomav <lipaavar@msn.com>
2023-12-26 15:55:50 +0100

    ...🎢

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,11 +1,12 @@
- 
 // Check if user has role
 
-export const hasRole = (role) => (req, res, next) => {
-  try {
-    if (req.user.role !== role) throw new Error();
-    next();
-  } catch (error) {
-    res.status(403).send({ message: "access dddddd" });
-  }
-};
+export const hasRole =
+  (...roles) =>
+  (req, res, next) => {
+    try {
+      if (!roles.includes(req.user.role)) throw new Error();
+      next();
+    } catch (error) {
+      res.status(403).send({ message: "access dddddd" });
+    }
+  };
