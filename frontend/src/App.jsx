ab59a70e36356276846846c938a8b584cbd91dcc
elena <ellena_purice@yahoo.com>
2024-01-12 09:53:54 +0100

    loader new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,9 @@
 import AppRoutes from "./routes";
-import { useLocation } from "react-router-dom";
-import Loader from "./components/loader/loader.jsx";
-import { useEffect, useState } from "react";
 
 const App = () => {
-  const [showLoader, setShowLoader] = useState(true);
-  const location = useLocation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowLoader(false);
-    }, 3000);
-  }, []);
-
-  const renderLoader = () => {
-    const { pathname } = location;
-
-    if (showLoader && pathname === "/") {
-      return <Loader />;
-    }
-    return null;
-  };
-
   return (
     <div className="App">
-      {renderLoader()}
-      {!showLoader && <AppRoutes />}
+      <AppRoutes />
     </div>
   );
 };
