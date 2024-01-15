c4741969fbae22bf548b04da2f1d56df19923ea0
Nerezea <nerezea@outlook.de>
2024-01-15 10:31:25 +0100

    removed header

--- frontend/src/pages/landing/elenaHeader.jsx
+++ frontend/src/pages/landing/elenaHeader.jsx
@@ -5,7 +5,6 @@ const header = () => {
   return (
     <nav className="header-wrapper">
       <iframe src="https://lottie.host/embed/46e1efc7-148b-4537-92d0-1054c1188603/dGAswJ2qhq.json"></iframe>
-      <h1>HiSchool</h1>
       <div className="nav-wrapper">
         <div className="navigation">
           <NavLink to="/login" className="activeClass">
