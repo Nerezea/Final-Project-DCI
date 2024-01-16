1f49e86a70cfb97c8f99588318ed38f45190d90a
newmomav <lipaavar@msn.com>
2024-01-16 11:12:15 +0100

    color forum§

--- frontend/src/pages/forum copy/components/message.jsx
+++ frontend/src/pages/forum copy/components/message.jsx
@@ -21,7 +21,7 @@ const Message = ({
       <Avatar src={userImage}></Avatar>
       <div
         className={style.bubble}
-        style={{ background: isSender ? "#fbf98c90" : "#dee8f452" }}
+        style={{ background: isSender ? "#fbf98c" : "#f0f5fc" }}
       >
         <span
           className={style.sender}
